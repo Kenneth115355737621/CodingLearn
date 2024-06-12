3c35d2560e085c2752dfabf67f82b8e39a7cee00
Kenneth115355737621 <kennethxie6@gmail.com>
2024-06-12 20:22:51 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String text = "Hello You!";
-        System.out.println(text);
-        String text1 = "Time to play Infinite Chef!";
-        System.out.println(text1);
-        int power = 10;
-        System.out.println("Mario's power is: " + power);
+        
     }
+        
 }
