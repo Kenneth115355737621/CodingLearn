574fee8f81ec1b20dbef5de5c675d9cbe46b9839
Kenneth115355737621 <kennethxie6@gmail.com>
2024-06-12 21:18:53 +0800

    send it

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        
+        int number = exponential(5, 5);
+        System.out.println(number);
     }
-        
+    public static int exponential(int base, int num){
+        int rNum = base;
+        for (int i = 1; i < num; i++){
+            rNum = rNum*base;
+        }
+        return rNum;
+    }
+
 }
